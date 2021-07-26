3e06e0cfefd349c9b021160dc867eea02358f662
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-07-26 18:33:00 +0300

    Fix

--- src/main/java/com/epam/jwd/aircraft/CargoAircraft.java
+++ src/main/java/com/epam/jwd/aircraft/CargoAircraft.java
@@ -1,9 +1,37 @@
 package com.epam.jwd.aircraft;
 
+import java.util.Objects;
+
 public class CargoAircraft extends Aircraft {
 
+
+    private String typesOfTransportedCargo;
     public CargoAircraft(String modelName, float liftingCapacityInKilograms,
-                         float fuelConsumption, int rangeOfFlightInKilometers) {
+                         float fuelConsumption, int rangeOfFlightInKilometers, String typesOfTransportedCargo) {
         super(modelName, 0, liftingCapacityInKilograms, fuelConsumption, rangeOfFlightInKilometers);
+        this.typesOfTransportedCargo = typesOfTransportedCargo;
+    }
+
+    public String getTypesOfTransportedCargo() {
+
+        return typesOfTransportedCargo;
+    }
+
+    public void setTypesOfTransportedCargo(String typesOfTransportedCargo) {
+        this.typesOfTransportedCargo = typesOfTransportedCargo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CargoAircraft)) return false;
+        if (!super.equals(o)) return false;
+        CargoAircraft that = (CargoAircraft) o;
+        return Objects.equals(getTypesOfTransportedCargo(), that.getTypesOfTransportedCargo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getTypesOfTransportedCargo());
     }
 }
--- src/main/java/com/epam/jwd/factory/CISAircraftFactory.java
+++ src/main/java/com/epam/jwd/factory/CISAircraftFactory.java
@@ -7,48 +7,48 @@ public class CISAircraftFactory implements AircraftFactory {
     @Override
     public Aircraft createLongHaulPassengerAircraft() {
         return new PassengerAircraft("Boeing 747-300", 624, 13400,
-                10300, 14200);
+                10300, 14200, 8);
     }
 
     @Override
     public Aircraft createLongHaulCargoAircraft() {
         return new CargoAircraft("AH-225", 390000,
-                15900, 15400);
+                15900, 15400, "Mail");
     }
 
     @Override
     public Aircraft createTrainingAircraft() {
         return new TrainingAircraft("Hawker Siddeley Hawk",
-                920, 2520);
+                920, 2520, 1);
     }
 
     @Override
     public Aircraft createIntelligenceAircraft() {
         return new DedicatedAircraft("IL-80", 0,
-                8400, 10300);
+                8400, 10300, "Intelligence");
     }
 
     @Override
     public Aircraft createMiddleHaulPassengerAircraft() {
         return new PassengerAircraft("Boeing 737-800", 225,
-                4900, 3657, 4500);
+                4900, 3657, 4500, 10);
     }
 
     @Override
     public Aircraft createShortHaulPassengerAircraft() {
         return new PassengerAircraft("Sukhoi SuperJet 100", 120, 1200,
-                1240, 2400);
+                1240, 2400, 4);
     }
 
     @Override
     public Aircraft createMiddleHaulCargoAircraft() {
         return new CargoAircraft("Аn-24", 9000,
-                2470, 5700);
+                2470, 5700, "Glass");
     }
 
     @Override
     public Aircraft createRescueAircraft() {
         return new DedicatedAircraft("Be-200", 12370,
-                5600, 10500);
+                5600, 10500, "Rescue people");
     }
 }
